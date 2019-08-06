067e43c66bcf36ea98a6f252073bd04e010fe5f3
Grigorii Horos <horosgrisa@gmail.com>
2019-08-06 04:28:03 +0300

    Add bookmark for parrent dir

--- pr-cwd.plugin.zsh
+++ pr-cwd.plugin.zsh
@@ -8,16 +8,46 @@ if command -v zpm >/dev/null; then
   zpm zpm-zsh/colors
 fi
 
+is_bookmark_dir(){
+  
+  rpath=$(realpath $1)
+  
+  if [[ "$rpath" =~ ^"$HOME"(/|$) ]]; then
+    cwd="\$HOME${rpath#$HOME}"
+  else
+    cwd="$rpath"
+  fi
+  
+  if [[ ! -z "$BOOKMARKS_FILE" ]]; then
+    if grep -q -e "^$cwd|" "$BOOKMARKS_FILE"; then
+      return 0
+    fi
+    
+  fi
+  
+  return 1
+}
+
+get_bookmark(){
+  
+  rpath=$(realpath $1)
+  
+  if [[ "$rpath" =~ ^"$HOME"(/|$) ]]; then
+    cwd="\$HOME${rpath#$HOME}"
+  else
+    cwd="$rpath"
+  fi
+  
+  grep "$cwd" "$BOOKMARKS_FILE" | awk -F'|' '{print $2}'
+  
+}
+
 _pr_cwd_HOME_=$(echo $HOME | sed 's/\//\\\//g')
 
 if [[ $CLICOLOR = 1 ]]; then
-  
   bookmark_icon="%{$c[blue]$c_dim%}%{$c_reset%} "
-  
 else
-  
   bookmark_icon=" "
-  
 fi
 
 _pr_cwd() {
@@ -41,36 +71,54 @@ _pr_cwd() {
     
   fi
   
-  if [[ "$PWD" =~ ^"$HOME"(/|$) ]]; then
-    cwd="\$HOME${PWD#$HOME}"
-  else
-    cwd="$PWD"
+  
+  if is_bookmark_dir "$PWD" ; then
+    
+    if [[ $CLICOLOR = 1 ]]; then
+      
+      link=$'%{\033]8;;file://'"$PWD"$'\a%}'$(get_bookmark "$PWD")$'%{\033]8;;\a%}'
+      pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}${bookmark_icon}%{$c[cyan]$c_bold%}$link%{$c[reset]%}$CURRENT_PATH_SUFIX"
+      
+    else
+      
+      pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}${bookmark_icon}$(get_bookmark "$PWD")$CURRENT_PATH_SUFIX"
+      
+    fi
+    
+    return 0
   fi
   
-  if [[ ! -z "$BOOKMARKS_FILE" ]]; then
-    if grep -q -e "^$cwd|" "$BOOKMARKS_FILE"; then
-      bookmark_name=$(grep "$cwd" "$BOOKMARKS_FILE" | awk -F'|' '{print $2}')
+  if is_bookmark_dir "$PWD/.." ; then
+    
+    if [[ $CLICOLOR = 1 ]]; then
       
-      if [[ $CLICOLOR = 1 ]]; then
-        link=${bookmark_name//\//%{$c[red]$c[bold]%}\/%{$c[cyan]$c[bold]%}}
-        link=$'%{\033]8;;file://'"$PWD"$'\a%}'$link$'%{\033]8;;\a%}'
-        pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}${bookmark_icon}%{$c[cyan]$c_bold%}$link%{$c[reset]%}$CURRENT_PATH_SUFIX"
-      else
-        pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}${bookmark_icon}$bookmark_name$CURRENT_PATH_SUFIX"
-      fi
+      cwd="%{$c[cyan]$c_bold%}$(get_bookmark "$PWD/..")%{$c[red]$c[bold]%}/"
+      cwd+="%{$c[blue]$c_bold%}$(print -Pn %1~)%{$c[reset]%}"
+      link=$'%{\033]8;;file://'"$PWD"$'\a%}'${cwd}$'%{\033]8;;\a%}'
+      pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}${bookmark_icon}$link$CURRENT_PATH_SUFIX"
       
-      return 0
+    else
+      
+      cwd="$(get_bookmark "$PWD/..")/$(print -Pn %1~)"
+      pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}${bookmark_icon}${cwd}$CURRENT_PATH_SUFIX"
+      
     fi
+    
+    return 0
   fi
   
   newPWD=$(print -Pn %2~| sed 's/^'$_pr_cwd_HOME_'/~/g')
   
   if [[ $CLICOLOR = 1 ]]; then
+    
     link=${newPWD//\//%{$c[red]$c[bold]%}\/%{$c[blue]$c[bold]%}}
     link=$'%{\033]8;;file://'"$PWD"$'\a%}'$link$'%{\033]8;;\a%}'
     pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}%{$c[blue]$c[bold]%}$link$CURRENT_PATH_SUFIX%{$c[reset]%}"
+    
   else
+    
     pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}$newPWD$CURRENT_PATH_SUFIX"
+    
   fi
   
 }
