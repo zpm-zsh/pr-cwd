b675a44bb7958ea70b440e17f09a25fb9c67a8a4
Grigorii Horos <horosgrisa@gmail.com>
2019-08-06 03:05:13 +0300

    Add integration with jocelynmallon/zshmarks

--- pr-cwd.plugin.zsh
+++ pr-cwd.plugin.zsh
@@ -11,27 +11,56 @@ fi
 _pr_cwd_HOME_=$(echo $HOME | sed 's/\//\\\//g')
 
 _pr_cwd() {
-  
-  local newPWD=$(print -Pn %2~)
-  newPWD=$(echo $newPWD| sed 's/^'$_pr_cwd_HOME_'/~/g')
-  
-  pr_cwd_plain="$newPWD"
-
-  local lockIcon=""
-  if [[ ! -w "$PWD" ]]; then
-    lockIcon="%{$c[red]$c[dim]%}%{$c[reset]%} "
-  else
-	lockIcon="%{$c[green]$c[dim]%}%{$c[reset]%} "
-  fi
+  pr_cwd=''
   
   if [[ $CLICOLOR = 1 ]]; then
-    newPWD=${newPWD//\//%{$c[red]$c[bold]%}\/%{$c[blue]$c[bold]%}}
-    newPWD=$'%{\033]8;;file://'"$PWD"$'\a%}'$newPWD$'%{\033]8;;\a%}'
-    pr_cwd="$CURRENT_PATH_PREFIX$lockIcon%{$c[blue]$c[bold]%}$newPWD$CURRENT_PATH_SUFIX%{$c[reset]%}"
+    
+    if [[ ! -w "$PWD" ]]; then
+      lockIcon="%{$c[red]$c[dim]%}%{$c[reset]%} "
+    else
+      lockIcon="%{$c[green]$c[dim]%}%{$c[reset]%} "
+    fi
+    
+  else
+    
+    if [[ ! -w "$PWD" ]]; then
+      lockIcon=" "
+    else
+      lockIcon=" "
+    fi
+    
+  fi
+  
+  if [[ "$PWD" =~ ^"$HOME"(/|$) ]]; then
+    cwd="\$HOME${PWD#$HOME}"
+  fi
+  
+  if [[ ! -z "$BOOKMARKS_FILE" ]]; then
+    if grep -q -e "^$cwd|" "$BOOKMARKS_FILE"; then
+      bookmark_name=$(grep "$cwd" "$BOOKMARKS_FILE" | awk -F'|' '{print $2}')
+      
+      if [[ $CLICOLOR = 1 ]]; then
+        link=${bookmark_name//\//%{$c[red]$c[bold]%}\/%{$c[cyan]$c[bold]%}}
+        link=$'%{\033]8;;file://'"$PWD"$'\a%}'$link$'%{\033]8;;\a%}'
+        pr_cwd="$CURRENT_PATH_PREFIX$lockIcon%{$c[cyan]$c_bold%}$link%{$c[reset]%}$CURRENT_PATH_SUFIX"
+      else
+        pr_cwd="$CURRENT_PATH_PREFIX$lockIcon$bookmark_name$CURRENT_PATH_SUFIX"
+      fi
+      
+      return 0
+    fi
+  fi
+  
+  newPWD=$(print -Pn %2~| sed 's/^'$_pr_cwd_HOME_'/~/g')
+  
+  if [[ $CLICOLOR = 1 ]]; then
+    link=${newPWD//\//%{$c[red]$c[bold]%}\/%{$c[blue]$c[bold]%}}
+    link=$'%{\033]8;;file://'"$PWD"$'\a%}'$link$'%{\033]8;;\a%}'
+    pr_cwd="$CURRENT_PATH_PREFIX$lockIcon%{$c[blue]$c[bold]%}$link$CURRENT_PATH_SUFIX%{$c[reset]%}"
   else
     pr_cwd="$CURRENT_PATH_PREFIX$lockIcon$newPWD$CURRENT_PATH_SUFIX"
   fi
-
+  
 }
 
 _pr_cwd
