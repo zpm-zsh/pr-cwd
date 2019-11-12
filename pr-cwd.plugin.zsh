66ccdeaab92ce2e625f6c969341f813c1df8b583
Grigorii Horos <horosgrisa@gmail.com>
2019-11-12 14:01:05 +0200

    Optimize

--- pr-cwd.plugin.zsh
+++ pr-cwd.plugin.zsh
@@ -1,10 +1,3 @@
-#!/usr/bin/env zsh
-
-# Standarized $0 handling, following:
-# https://github.com/zdharma/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc
-0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
-_DIRNAME="${0:h}"
-
 CURRENT_PATH_PREFIX=${CURRENT_PATH_PREFIX:-" "}
 CURRENT_PATH_SUFIX=${CURRENT_PATH_SUFIX:-""}
 
@@ -15,11 +8,102 @@ if (( $+functions[zpm] )); then
   zpm zpm-zsh/helpers,inline zpm-zsh/colors,inline
 fi
 
-source  "${_DIRNAME}/home.zsh"
-source  "${_DIRNAME}/one-dir.zsh"
-source  "${_DIRNAME}/bookmark.zsh"
-source  "${_DIRNAME}/node.zsh"
-source  "${_DIRNAME}/rust.zsh"
+_pr_cwd_bookmark_icon="%{$c[blue]$c_dim%}%{$c_reset%} "
+
+_pr_cwd_is_bookmark_dir(){
+  rpath=$(print -D ${1:P})
+   
+  if [[ ! -z "$BOOKMARKS_FILE" ]]; then
+    declare -a lines; lines=( "${(@f)"$(<$BOOKMARKS_FILE)"}" )
+    declare -a grepped; grepped=( ${(M)lines:#${rpath}\|*} )
+
+    if [[ ! -z "$grepped" ]] then
+      return 0
+    fi
+    
+  fi
+  
+  return 1
+}
+
+_pr_cwd_get_bookmark(){
+  rpath=$(print -D ${1:P})
+
+  declare -a lines; lines=( "${(@f)"$(<$BOOKMARKS_FILE)"}" )
+  declare -a grepped; grepped=( ${(M)lines:#${rpath}\|*} )
+
+  echo -n "%{$c[cyan]$c_bold%}"
+  echo -n ${grepped##*\|}
+  echo -n "%{$c[reset]%}"
+}
+
+_pr_cwd_is_home_dir(){
+  [[ "$(print -Pn %1~ )" == '~'* ]]
+}
+
+_pr_cwd_get_home_dir(){
+  echo -n "%{$c[blue]$c_bold%}"
+  echo -n $(print -Pn %~)
+  echo -n "%{$c[reset]%}"
+}
+
+_pr_cwd_is_node_dir(){
+  rpath=${1:P}
+  
+  if [[ -f "${rpath}/package.json" ]] ; then
+    return 0
+  fi
+  
+  return 1
+}
+
+_pr_cwd_get_node_package(){
+  rpath=${1:P}
+  
+  package="${rpath}/package.json"
+  
+  package_str='Unknown name'
+  if (( $+commands[jq] )); then
+    package_str=$(jq -r '.name + "@" + .version' ${package} 2>/dev/null)
+  fi
+    
+  echo -n "%{$c[green]$c_bold%}"
+  echo -n "${package_str}"
+  echo -n "%{$c[reset]%}"
+}
+
+_pr_cwd_is_one_dir(){
+  [[ "${PWD:h}" == "/" ]]
+}
+
+_pr_cwd_get_one_dir(){
+  
+  echo -n "%{$c[red]${c_bold}%}/"
+  echo -n "%{$c[blue]${c_bold}%}${PWD:t}"
+  echo -n "%{$c[reset]%}"
+  
+}
+
+_pr_cwd_is_rust_dir(){
+  rpath=${1:P}
+  
+  if [[ -f "${rpath}/Cargo.toml" ]] ; then
+    return 0
+  fi
+  
+  return 1
+}
+
+_pr_cwd_get_rust_package(){
+  rpath=${1:P}
+    
+  local rust_version_str=$(
+    cargo metadata --no-deps --format-version 1 --manifest-path=${rpath}/Cargo.toml \
+    | jq --raw-output '(.packages[0] | .name) + "#" + (.packages[0] | .version)'
+  )
+  
+  echo -n "%{$c[magenta]$bold%}${rust_version_str}%{$c[reset]%}"  
+}
 
 _pr_cwd() {
   pr_cwd=''
