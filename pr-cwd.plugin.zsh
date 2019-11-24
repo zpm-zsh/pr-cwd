73a7656aa2100c2f7637d81760512bc302cbcbe3
Grigorii Horos <horosgrisa@gmail.com>
2019-11-24 16:58:27 +0200

    Fixes

--- pr-cwd.plugin.zsh
+++ pr-cwd.plugin.zsh
@@ -1,3 +1,5 @@
+#!/usr/bin/env zsh
+
 CURRENT_PATH_PREFIX=${CURRENT_PATH_PREFIX:-" "}
 CURRENT_PATH_SUFIX=${CURRENT_PATH_SUFIX:-""}
 
@@ -8,16 +10,16 @@ if (( $+functions[zpm] )); then
   zpm zpm-zsh/helpers,inline zpm-zsh/colors,inline
 fi
 
-_pr_cwd_bookmark_icon="%{$c[blue]$c_dim%}%{$c_reset%} "
+_pr_cwd_bookmark_icon="%{${c[blue]}${c_dim}%}%{${c_reset}%} "
 
 _pr_cwd_is_bookmark_dir(){
-  rpath=$(print -D ${1:P})
+  rpath="$(print -D ${1:P})"
   
-  if [[ ! -z "$BOOKMARKS_FILE" ]]; then
+  if [[ -n "$BOOKMARKS_FILE" ]]; then
     declare -a lines; lines=( "${(@f)"$(<$BOOKMARKS_FILE)"}" )
-    declare -a grepped; grepped=( ${(M)lines:#${rpath}\|*} )
+    declare -a grepped; grepped=( "${(M)lines:#${rpath}\|*}" )
     
-    if [[ ! -z "$grepped" ]] then
+    if [[ -n "$grepped" ]]; then
       return 0
     fi
   fi
@@ -30,7 +32,7 @@ _pr_cwd_is_home_dir(){
 }
 
 _pr_cwd_is_node_dir(){
-  rpath=${1:P}
+  rpath="${1:P}"
   
   if [[ -f "${rpath}/package.json" ]] ; then
     return 0
@@ -44,7 +46,7 @@ _pr_cwd_is_one_dir(){
 }
 
 _pr_cwd_is_rust_dir(){
-  rpath=${1:P}
+  rpath="${1:P}"
   
   if [[ -f "${rpath}/Cargo.toml" ]] ; then
     return 0
@@ -54,52 +56,52 @@ _pr_cwd_is_rust_dir(){
 }
 
 _pr_cwd_get_bookmark(){
-  rpath=$(print -D ${1:P})
+  rpath="$(print -D ${1:P})"
   
   declare -a lines; lines=( "${(@f)"$(<$BOOKMARKS_FILE)"}" )
-  declare -a grepped; grepped=( ${(M)lines:#${rpath}\|*} )
+  declare -a grepped; grepped=( "${(M)lines:#${rpath}\|*}" )
   
-  echo -n "%{$c[cyan]$c_bold%}"
-  echo -n ${grepped##*\|}
-  echo -n "%{$c[reset]%}"
+  echo -n "%{${c[cyan]}${c_bold}%}"
+  echo -n "${grepped##*\|}"
+  echo -n "%{${c_reset}%}"
 }
 
 _pr_cwd_get_home_dir(){
-  echo -n "%{$c[blue]$c_bold%}"
-  echo -n $(print -Pn %~)
-  echo -n "%{$c[reset]%}"
+  echo -n "%{${c[blue]}${c_bold}%}"
+  echo -n "$(print -Pn %~)"
+  echo -n "%{${c_reset}%}"
 }
 
 _pr_cwd_get_one_dir(){
-  echo -n "%{$c[red]${c_bold}%}/"
-  echo -n "%{$c[blue]${c_bold}%}${PWD:t}"
-  echo -n "%{$c[reset]%}"
+  echo -n "%{${c[red]}${c_bold}%}/"
+  echo -n "%{${c[blue]}${c_bold}%}${PWD:t}"
+  echo -n "%{${c_reset}%}"
 }
 
 _pr_cwd_get_node_package(){
-  rpath=${1:P}
+  rpath="${1:P}"
   package="${rpath}/package.json"
   package_str='Unknown name'
   
   if (( $+commands[jq] )); then
-    package_str=$(jq -r ".name + \"%{${c[grey]}%}@%{${c[bright_grey]}%}\" + .version" ${package} 2>/dev/null)
+    package_str="$(jq -r ".name + \"%{${c[grey]}%}@%{${c[bright_grey]}%}\" + .version" "${package}" 2>/dev/null)"
   fi
   
-  echo -n "%{$c[green]$c_bold%}"
+  echo -n "%{${c[green]}${c_bold}%}"
   echo -n "${package_str}"
-  echo -n "%{$c[reset]%}"
+  echo -n "%{${c_reset}%}"
 }
 
 _pr_cwd_get_rust_package(){
-  rpath=${1:P}
+  rpath="${1:P}"
   package_str='Unknown name'
   
   if (( $+commands[jq] )); then
-    package_str=$(cargo metadata --no-deps --format-version 1 --manifest-path=${rpath}/Cargo.toml \
-    | jq --raw-output "(.packages[0] | .name) + \"%{${c[grey]}%}#%{${c[bright_grey]}%}\" + (.packages[0] | .version)"  2>/dev/null  )
+    package_str="$(cargo metadata --no-deps --format-version 1 --manifest-path="${rpath}/Cargo.toml" \
+    | jq --raw-output "(.packages[0] | .name) + \"%{${c[grey]}%}#%{${c[bright_grey]}%}\" + (.packages[0] | .version)"  2>/dev/null  )"
   fi
   
-  echo -n "%{$c[magenta]$c_bold%}${package_str}%{$c[reset]%}"
+  echo -n "%{${c[magenta]}${c_bold}%}${package_str}%{${c_reset}%}"
 }
 
 _pr_cwd() {
@@ -107,7 +109,7 @@ _pr_cwd() {
   
   # Prepare ----
   if [[ ! -w "$PWD" ]]; then
-    lock_icon="%{$c[red]$c[dim]%}%{$c[reset]%} "
+    lock_icon="%{${c[red]}${c_dim}%}%{${c_reset}%} "
   else
     lock_icon=""
   fi
@@ -169,11 +171,11 @@ _pr_cwd() {
     return 0
   fi
   
-  newPWD=$(print -Pn %2~)
+  newPWD="$(print -Pn %2~)"
   
-  link=${newPWD//\//%{$c[red]$c[bold]%}\/%{$c[blue]$c[bold]%}}
-  link=$(hyperlink-file-pr ${link} ${PWD})
-  pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}%{$c[blue]$c[bold]%}$link$CURRENT_PATH_SUFIX%{$c[reset]%}"
+  link=${newPWD//\//%{${c[red]}${c_bold}%}\/%{${c[blue]}${c_bold}%}}
+  link=$(hyperlink-file-pr "${link}" "${PWD}")
+  pr_cwd="${CURRENT_PATH_PREFIX}${lock_icon}%{${c[blue]}${c_bold}%}${link}${CURRENT_PATH_SUFIX}%{${c_reset}%}"
 }
 
 _pr_cwd
