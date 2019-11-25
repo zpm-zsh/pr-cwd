6d4760979ea35e159270c8bf7e905aca6eec1796
Grigorii Horos <horosgrisa@gmail.com>
2019-11-25 10:46:12 +0200

    Optimizations

--- pr-cwd.plugin.zsh
+++ pr-cwd.plugin.zsh
@@ -12,77 +12,82 @@ fi
 
 _pr_cwd_bookmark_icon="%{${c[blue]}${c_dim}%}%{${c_reset}%} "
 
-_pr_cwd_is_bookmark_dir(){
-  rpath="$(print -D ${1:P})"
-   
-  if [[ ! -z "$BOOKMARKS_FILE" ]]; then
-    declare -a lines; lines=( "${(@f)"$(<$BOOKMARKS_FILE)"}" )
-    declare -a grepped; grepped=( ${(M)lines:#${rpath}\|*} )
-
-    if [[ ! -z "${grepped}" ]] then
-      return 0
-    fi
-  fi
-  
-  return 1
-}
-
-_pr_cwd_is_home_dir(){
-  [[ "$(print -Pn %1~ )" == '~'* ]]
-}
-
-_pr_cwd_is_node_dir(){
-  rpath="${1:P}"
-  
-  if [[ -f "${rpath}/package.json" ]] ; then
+_pr_cwd_home(){
+  if [[ "$(print -Pn %1~ )" != '~'* ]]; then
+    echo
+    return 1
+  else
+    echo -n "%{${c[blue]}${c_bold}%}"
+    echo -n "$(print -Pn %~)"
+    echo -n "%{${c_reset}%}"
+    
     return 0
   fi
-  
-  return 1
 }
 
-_pr_cwd_is_one_dir(){
-  [[ "${PWD:h}" == "/" ]]
-}
-
-_pr_cwd_is_rust_dir(){
-  rpath="${1:P}"
-  
-  if [[ -f "${rpath}/Cargo.toml" ]] ; then
+_pr_cwd_one(){
+  if [[ "${PWD:h}" != "/" ]]; then
+    echo
+    return 1
+  else
+    
+    echo -n "%{${c[red]}${c_bold}%}/"
+    echo -n "%{${c[blue]}${c_bold}%}${PWD:t}"
+    echo -n "%{${c_reset}%}"
+    
     return 0
   fi
-  
-  return 1
 }
 
 _pr_cwd_get_bookmark(){
   rpath="$(print -D ${1:P})"
-
-  declare -a lines; lines=( "${(@f)"$(<$BOOKMARKS_FILE)"}" )
-  declare -a grepped; grepped=( ${(M)lines:#${rpath}\|*} )
-
-  echo -n "%{${c[cyan]}${c_bold}%}"
-  echo -n ${grepped##*\|}
-  echo -n "%{${c_reset}%}"
+  
+  if [[ ! -z "$BOOKMARKS_FILE" ]]; then
+    declare -a lines; lines=( "${(@f)"$(<$BOOKMARKS_FILE)"}" )
+    declare -a grepped; grepped=( ${(M)lines:#${rpath}\|*} )
+    
+    if [[ ! -z "${grepped}" ]] then
+      echo -n "%{${c[cyan]}${c_bold}%}"
+      echo -n ${grepped##*\|}
+      echo -n "%{${c_reset}%}"
+      
+      return 0
+    fi
+  fi
+  
+  echo
+  return 1
 }
 
-
-_pr_cwd_get_home_dir(){
-  echo -n "%{${c[blue]}${c_bold}%}"
-  echo -n "$(print -Pn %~)"
-  echo -n "%{${c_reset}%}"
+_pr_cwd_rust(){
+  rpath="${1:P}"
+  package_str='Unknown name'
+  
+  if [[ ! -f "${rpath}/Cargo.toml" ]] ; then
+    echo
+    return 1
+  fi
+  
+  if (( $+commands[jq] )); then
+    package_str="$(
+      cargo metadata --no-deps --format-version 1 --manifest-path="${rpath}/Cargo.toml" \
+      | jq --raw-output "(.packages[0] | .name) + '%{${c[grey]}%}#%{${c[bright_grey]}%}' + (.packages[0] | .version)"  2>/dev/null
+    )"
+  fi
+  
+  echo -n "%{${c[magenta]}${c_bold}%}${package_str}%{${c_reset}%}"
+  return 0
 }
 
-_pr_cwd_get_one_dir(){
-  echo -n "%{${c[red]}${c_bold}%}/"
-  echo -n "%{${c[blue]}${c_bold}%}${PWD:t}"
-  echo -n "%{${c_reset}%}"
-}
-
-_pr_cwd_get_node_package(){
+_pr_cwd_node(){
   rpath="${1:P}"
   package="${rpath}/package.json"
   package_str='Unknown name'
+  
+  if [[ ! -f "${package}" ]] ; then
+    echo
+    return 1
+  fi
   
   if (( $+commands[jq] )); then
     package_str="$(jq -r ".name + \"%{${c[grey]}%}@%{${c[bright_grey]}%}\" + .version" "${package}" 2>/dev/null)"
@@ -91,18 +96,8 @@ _pr_cwd_get_node_package(){
   echo -n "%{${c[green]}${c_bold}%}"
   echo -n "${package_str}"
   echo -n "%{${c_reset}%}"
-}
-
-_pr_cwd_get_rust_package(){
-  rpath="${1:P}"
-  package_str='Unknown name'
   
-  if (( $+commands[jq] )); then
-    package_str="$(cargo metadata --no-deps --format-version 1 --manifest-path="${rpath}/Cargo.toml" \
-    | jq --raw-output "(.packages[0] | .name) + \"%{${c[grey]}%}#%{${c[bright_grey]}%}\" + (.packages[0] | .version)"  2>/dev/null  )"
-  fi
-  
-  echo -n "%{${c[magenta]}${c_bold}%}${package_str}%{${c_reset}%}"
+  return 0
 }
 
 _pr_cwd() {
@@ -116,60 +111,80 @@ _pr_cwd() {
   fi
   # /Prepare ----
   
-  if _pr_cwd_is_bookmark_dir "$PWD" ; then
+  local bookmark_dir=$(_pr_cwd_get_bookmark "$PWD")
+  if [[ -n "${bookmark_dir}" ]] ; then
     pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}${_pr_cwd_bookmark_icon}$(
-      hyperlink-file-pr "$(_pr_cwd_get_bookmark "$PWD")" "$PWD"
+      hyperlink-file-pr "${bookmark_dir}" "$PWD"
     )$CURRENT_PATH_SUFIX"
     return 0
   fi
   
-  if (( $+commands[node] )) && _pr_cwd_is_node_dir "$PWD" ; then
+  if (( $+commands[node] )); then;
+    local node_dir=$(_pr_cwd_node "$PWD")
+    
+    if [[ -n "${node_dir}" ]] ; then
+      pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}$(
+        hyperlink-file-pr "${node_dir}" "$PWD"
+      )$CURRENT_PATH_SUFIX"
+      return 0
+    fi
+  fi
+  
+  if (( $+commands[rustc] )) ;then
+    local rust_dir=$(_pr_cwd_rust "$PWD")
+    
+    if [[ -n "${rust_dir}" ]] ; then
+      pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}$(
+        hyperlink-file-pr "${rust_dir}" "$PWD"
+      )$CURRENT_PATH_SUFIX"
+      return 0
+    fi
+  fi
+  
+  local home_dir="$(_pr_cwd_home $PWD)"
+  if [[ -n "${home_dir}" ]]; then
     pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}$(
-      hyperlink-file-pr "$(_pr_cwd_get_node_package "$PWD")" "$PWD"
+      hyperlink-file-pr "${home_dir}" "$PWD"
     )$CURRENT_PATH_SUFIX"
     return 0
   fi
   
-  if (( $+commands[rustc] )) && _pr_cwd_is_rust_dir "$PWD" ; then
+  local one_dir="$(_pr_cwd_one)"
+  if [[ -n "${one_dir}" ]] ; then
     pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}$(
-      hyperlink-file-pr "$(_pr_cwd_get_rust_package "$PWD")" "$PWD"
+      hyperlink-file-pr "${one_dir}" "$PWD"
     )$CURRENT_PATH_SUFIX"
     return 0
   fi
   
-  if _pr_cwd_is_home_dir ; then
-    pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}$(
-      hyperlink-file-pr "$(_pr_cwd_get_home_dir)" "$PWD"
-    )$CURRENT_PATH_SUFIX"
-    return 0
-  fi
-  
-  if _pr_cwd_is_one_dir ; then
-    pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}$(
-      hyperlink-file-pr "$(_pr_cwd_get_one_dir)" "$PWD"
-    )$CURRENT_PATH_SUFIX"
-    return 0
-  fi
-  
-  if _pr_cwd_is_bookmark_dir "$PWD/.." ; then
+  local bookmark_dir=$(_pr_cwd_get_bookmark "$PWD/..")
+  if [[ -n "${bookmark_dir}" ]]; then
     pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}${_pr_cwd_bookmark_icon}$(
-      hyperlink-file-pr "$(_pr_cwd_get_bookmark "$PWD/..")$(_pr_cwd_get_one_dir)" "$PWD"
+      hyperlink-file-pr "${bookmark_dir}$(_pr_cwd_get_one_dir)" "$PWD"
     )$CURRENT_PATH_SUFIX"
     return 0
   fi
   
-  if (( $+commands[node] )) && _pr_cwd_is_node_dir "$PWD/.." ; then
-    pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}$(
-      hyperlink-file-pr "$(_pr_cwd_get_node_package "$PWD/..")$(_pr_cwd_get_one_dir)" "$PWD"
-    )$CURRENT_PATH_SUFIX"
-    return 0
+  if (( $+commands[node] )) ; then
+    local node_dir=$(_pr_cwd_node "$PWD/..")
+    
+    if [[ -n "${node_dir}" ]] ; then
+      pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}$(
+        hyperlink-file-pr "${node_dir}$(_pr_cwd_get_one_dir)" "$PWD"
+      )$CURRENT_PATH_SUFIX"
+      return 0
+    fi
   fi
   
-  if (( $+commands[rustc] )) && _pr_cwd_is_rust_dir "$PWD/.." ; then
-    pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}$(
-      hyperlink-file-pr "$(_pr_cwd_get_rust_package "$PWD/..")$(_pr_cwd_get_one_dir
-    )" "$PWD" )$CURRENT_PATH_SUFIX"
-    return 0
+  if (( $+commands[rustc] )) ; then
+    local rust_dir=$(_pr_cwd_rust "$PWD/..")
+    
+    if [[ -n "${rust_dir}" ]] ; then
+      pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}$(
+        hyperlink-file-pr "${rust_dir}$(_pr_cwd_get_one_dir)" "$PWD"
+      )$CURRENT_PATH_SUFIX"
+      return 0
+    fi
   fi
   
   newPWD="$(print -Pn %2~)"
