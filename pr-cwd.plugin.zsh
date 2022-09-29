e5518877dfe328ef43db2b66ae206630e4ba5b55
Grigorii Horos <horosgrisa@gmail.com>
2022-09-29 17:39:39 +0300

    Change function

--- pr-cwd.plugin.zsh
+++ pr-cwd.plugin.zsh
@@ -8,79 +8,36 @@ DEPENDENCES_ARCH+=(jq)
 
 typeset -g pr_cwd
 
-typeset -g _pr_cwd_bookmark_icon="%{${c[blue]}${c[dim]}%}%{${c[reset]}%} "
-
 if [[ $PMSPEC != *f* ]] {
   fpath+=( "${0:h}/functions" )
 }
 
-autoload -Uz pr_cwd_get_bookmark pr_cwd_get_one_dir pr_cwd_get_home_dir  
+autoload -Uz pr_cwd_get_bookmark pr_cwd_get_one_dir pr_cwd_is_special pr_cwd_get_regular_dir
 
 function _pr_cwd() {
-  local lock_icon
-  local newPWD
-  local link
+  local local_cwd=''
 
-  if [[ ! -w "$PWD" ]]; then
-    lock_icon="%{${c[red]}${c[dim]}%}%{${c[reset]}%} "
+  last_part="$(pr_cwd_is_special $PWD)"
+  if [[ -n "$last_part" ]]; then
+    local_cwd="$last_part"
   else
-    lock_icon=""
-  fi
+    last_part="$(pr_cwd_get_regular_dir $PWD)"
+    first_part="$(pr_cwd_is_special "${PWD:h}")" 
+    if [[ -n "$first_part" ]]; then
+      local_cwd="${first_part}%{${c[red]}${c[bold]}%}/%{${c[reset]}%}${last_part}"
+    else
+      first_part="$(pr_cwd_get_regular_dir ${PWD:h})"
 
-  if [[ "$PWD" == "$HOME" ]]; then
-    local home_dir="$(pr_cwd_get_home_dir $PWD)"
+      local is_root=''
+      if [[ "${PWD:h}" == '/'* && "${PWD:h}" != '/'*'/'* && -n "${first_part}" ]]; then
+        local is_root="%{${c[red]}${c[bold]}%}/%{${c[reset]}%}"
+      fi
 
-    pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}$(
-      hyperlink-file-pr "${home_dir}" "$PWD"
-    )$CURRENT_PATH_SUFIX"
-    return 0
-  fi
-
-  if [[ -e "${BOOKMARKS_FILE:-"$HOME/.bookmarks"}" ]]; then
-    local bookmark_dir=$(pr_cwd_get_bookmark "$PWD")
-
-    if [[ -n "${bookmark_dir}" ]] ; then
-      pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}${_pr_cwd_bookmark_icon}$(
-        hyperlink-file-pr "${bookmark_dir}" "$PWD"
-      )$CURRENT_PATH_SUFIX"
-      return 0
+      local_cwd="${is_root}${first_part}%{${c[red]}${c[bold]}%}/%{${c[reset]}%}${last_part}"
     fi
   fi
 
-  if [[ "$(print -Pn %1~ )" == '~'* ]]; then
-    local home_dir="$(pr_cwd_get_home_dir $PWD)"
-
-    pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}$(
-      hyperlink-file-pr "${home_dir}" "$PWD"
-    )$CURRENT_PATH_SUFIX"
-    return 0
-  fi
-
-  if [[ "${PWD:h}" == "/" ]]; then
-    local one_dir="$(pr_cwd_get_one_dir)"
-
-    pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}$(
-      hyperlink-file-pr "${one_dir}" "$PWD"
-    )$CURRENT_PATH_SUFIX"
-    return 0
-  fi
-
-  if [[ -e "${BOOKMARKS_FILE:-"$HOME/.bookmarks"}" ]]; then
-    local bookmark_dir=$(pr_cwd_get_bookmark "$PWD/..")
-
-    if [[ -n "${bookmark_dir}" ]]; then
-      pr_cwd="$CURRENT_PATH_PREFIX${lock_icon}${_pr_cwd_bookmark_icon}$(
-        hyperlink-file-pr "${bookmark_dir}$(pr_cwd_get_one_dir)" "$PWD"
-      )$CURRENT_PATH_SUFIX"
-      return 0
-    fi
-  fi
-
-  newPWD="$(print -Pn %2~)"
-
-  link=${newPWD//\//%{${c[red]}${c[bold]}%}\/%{${c[blue]}${c[bold]}%}}
-  link=$(hyperlink-file-pr "${link}" "${PWD}")
-  pr_cwd="${CURRENT_PATH_PREFIX}${lock_icon}%{${c[blue]}${c[bold]}%}${link}${CURRENT_PATH_SUFIX}%{${c[reset]}%}"
+  pr_cwd="${CURRENT_PATH_PREFIX}${lock_icon}%{${c[blue]}${c[bold]}%}${local_cwd}${CURRENT_PATH_SUFIX}%{${c[reset]}%}"
 }
 
 autoload -Uz add-zsh-hook
